40118ed34588e7801be10c742dd89dc80c4a32bf
PotatoGuy-04 <m4fo465com.n@gmail.com>
2025-10-02 13:30:44 +0200

    Implement les2 Oefenbundel1

--- lab2 - GameStore/Controllers/PeopleController.cs
+++ lab2 - GameStore/Controllers/PeopleController.cs
@@ -2,6 +2,8 @@
 using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.DataAnnotations;
+using Microsoft.EntityFrameworkCore.ChangeTracking;
+using Microsoft.EntityFrameworkCore.Update.Internal;
 
 namespace lab2___GameStore;
 
@@ -28,5 +30,40 @@ public class PeopleController : ControllerBase
         context.People.Add(p);
         context.SaveChanges();
         return NoContent();
+    }
+    [HttpDelete("{id}")]
+    public IActionResult DeleteOne(int id)
+    {
+        var p = context.People.Find(id);
+
+        if (p == null)
+            return NotFound();
+    
+        context.People.Remove(p);
+        context.SaveChanges();
+        return NoContent();
+    }
+    [HttpGet("{id}")]
+    public IActionResult GetOne(int id)
+    {
+        return Ok(context.People.Find(id));
+    }
+    [HttpPut("{id}")]
+    public IActionResult UpdateOne(int id, [FromBody] Person updatedP)
+    {
+        var p = context.People.Find(id);
+        if (p == null)
+            return NotFound();
+        UpdatePerson(p, updatedP);
+
+        context.SaveChanges();
+        return NoContent();
+    }
+    private void UpdatePerson(Person p, Person updatedP)
+    {
+        p.FirstName = updatedP.FirstName;
+        p.LastName = updatedP.LastName;
+        p.Gender = updatedP.Gender;
+        p.Email = updatedP.Email;
     }
 }
--- lab2 - GameStore/obj/Debug/net9.0/lab2 - GameStore.AssemblyInfo.cs
+++ lab2 - GameStore/obj/Debug/net9.0/lab2 - GameStore.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("lab2 - GameStore")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c51f9707fd23c26a0692bf4711f1e9700ea4ebc5")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e4f45912d8006eaf24d281238ae0ee7d4d0433c5")]
 [assembly: System.Reflection.AssemblyProductAttribute("lab2 - GameStore")]
 [assembly: System.Reflection.AssemblyTitleAttribute("lab2 - GameStore")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
